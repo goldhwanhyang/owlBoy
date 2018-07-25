d120d91af0ba58db5ab7b59147df207d741f628c
박동건 <parkdg1010@gmail.com>
2018-07-26 08:57:14 +0900

    클래스 업데이트 : 불릿

--- bullet.cpp
+++ bullet.cpp
@@ -3,17 +3,36 @@
 HRESULT bullet::init(const char * imageName, int bulletMax, float range)
 {
 	_image = IMAGEMANAGER->findImage(imageName);
+	_bulletMax = bulletMax;
+	_range = range;
+
 	return S_OK;
 }
 
 void bullet::update()
 {
+
 }
 
 void bullet::render()
 {
+
 }
 
 void bullet::release()
+{
+
+}
+
+void bullet::fire()
+{
+
+}
+
+void bullet::move()
+{
+}
+
+void bullet::collide()
 {
 }
--- bullet.h
+++ bullet.h
@@ -2,13 +2,23 @@
 #include "actor.h"
 class bullet : public actor
 {
-	image* _image; //불릿이미지
+	image* _image;	// 불릿이미지
+	int _bulletMax; // 불릿개수
+	float _range;	// 불릿사거리
+
+	vector<bullet*> _vBullet;
+	vector<bullet*>::iterator _viBullet;
 
 public:
 	virtual HRESULT init(const char* imageName, int bulletMax, float range);
 	virtual void update();
 	virtual void render();
 	virtual void release();
+
+	void fire();
+	virtual void move();
+	virtual void collide();
+	vector<bullet*> getvBullet() { return _vBullet; }
 
 	bullet() {}
 	~bullet() {}
