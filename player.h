02298bf720ac64d07a47eb70744dc6cce187de17
DESKTOP-JSGIJ6O\goldh <goldhwanhyang@gmail.com>
2018-07-26 20:28:55 +0900

    플레이어 구르기 이미지,기능 추가

--- player.h
+++ player.h
@@ -2,15 +2,18 @@
 #include "actor.h"
 
 #define OTUS_WIDTH 50
-#define OTUS_HEIGTH 120
+#define OTUS_HEIGTH 125
 #define PI 3.14
-#define STATE_MAX 6
-enum STATE { IDLE , WALK, JUMP, JUMPFALL , FLY , FLYDOWN };
+#define STATE_MAX 7
+enum STATE { IDLE , WALK, JUMP, JUMPFALL , FLY , FLYDOWN, ROLL };
 
 class player : public actor
 {
 private:
 	STATE _state;
+	STATE _beforeState;
+
+
 	image* img[STATE_MAX];
 	image* _mapPixel;
 	image* _map;
@@ -19,7 +22,11 @@ private:
 	bool _isWalk;
 	bool _isJump;
 	bool _isFly;
+	bool _isFlyDown;
+	int _directionX;
+	int _directionY;
 	int _jumpCount;
+
 public:
 	HRESULT init();
 	void update();
@@ -28,7 +35,9 @@ public:
 
 	void setMap(image *map) { _map = map; }
 	void setMapPixel(image *mapPixel) { _mapPixel = mapPixel; }
-
+	void collide();
+	void inputKey();
+	void frameSetting();
 	player(){}
 	~player(){}
 };
